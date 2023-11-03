0716fd065063739ef0ac2dff28d5990c271db3be
prathmesh-bendal <42838449+prathmesh-bendal@users.noreply.github.com>
2023-11-03 12:31:09 +0530

    Time: 32 ms (28.11%), Space: 89.7 MB (53.10%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -8,47 +8,53 @@ public:
         if(n>m)
             return findMedianSortedArrays(nums2,nums1);
         
-        int count=n+m;
-        int left=0,right=n;
         
-        while(left<=right)
+        int l=0,r=n;
+        
+        while(l<=r)
         {
-            int cut1=(left+right)/2;
-            int cut2=count/2-cut1;
+            int n1=(l+r)/2;
+            int n2=(n+m+1)/2-n1;
             
-            int l1=INT_MIN;
-            if(cut1>0)
-                l1=nums1[cut1-1];
-            int r1=INT_MAX;
-            if(cut1<n)
-                r1=nums1[cut1];
+            int l1=INT_MIN,r1=INT_MIN;
+            int l2=INT_MAX,r2=INT_MAX;
             
-            int l2=INT_MIN;
-            if(cut2>0)
-                l2=nums2[cut2-1];
-            int r2=INT_MAX;
-            if(cut2<m)
-                r2=nums2[cut2];
+            if(n1-1>=0)
+                l1=nums1[n1-1];
+            if(n2-1>=0)
+                r1=nums2[n2-1];
             
-            if(l1<=r2 && l2<=r1)
+            if(n1<n)
+                l2=nums1[n1];
+            
+            if(n2<m)
+                r2=nums2[n2];
+            
+            if(l1<=r2 && r1<=l2)
             {
-                int op1=max(l1,l2);
-                int op2=min(r1,r2);
-                
-                if(count%2==1)
-                    return op2;
-                
-                return (op1+op2)/2.0000000;
+                if((n+m)%2==0)
+                {
+                    int lmax=max(l1,r1);
+                    int rmin=min(l2,r2);
+                    
+                    return (lmax+rmin)/2.0000000;
+                }
+                else
+                {
+                    return max(l1,r1)*1.0000;
+                }
             }
-            else if(l1>r2)
+            
+            if(l1>r2)
             {
-                right=cut1-1;
+                r=n1-1;
             }
-            else if(l2>r1)
+            if(r1>l2)
             {
-                left=cut1+1;
+                l=n1+1;
             }
         }
-        return -1;
+        
+        return -1.0000;
     }
 };
