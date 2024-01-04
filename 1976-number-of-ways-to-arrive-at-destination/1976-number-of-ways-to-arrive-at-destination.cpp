8b3dc2b29f924bed3cf23b272ca717593f5f90b5
prathmesh-bendal <42838449+prathmesh-bendal@users.noreply.github.com>
2024-01-04 18:52:36 +0530

    Time: 94 ms (13.59%), Space: 36.3 MB (6.46%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,55 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        int mod=1e9+7;
-        vector<pair<long long,long long>> adj[n];
-        for(auto it : roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+        
+        unordered_map<int,list<pair<int,int>>> adj;
+        
+        for(auto road:roads)
+        {
+            adj[road[0]].push_back({road[1],road[2]});
+            adj[road[1]].push_back({road[0],road[2]});
         }
-
-        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<>> pq;
-        // queue<pair<long long,long long>> pq;
-        vector<long long > dist(n,LLONG_MAX);
-        vector<long long> no_of_ways(n,0);
-        pq.push({0,0});
+        
+        vector<long long> dist(n,LLONG_MAX);
+        vector<int> count(n,0);
+        
         dist[0]=0;
-        no_of_ways[0]=1;
-        while(!pq.empty()){
-            long long node=pq.top().second;
-            long long d=pq.top().first;
+        count[0]=1;
+        
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
+        
+        pq.push({0,0});
+        int m=1e9+7;
+        while(pq.size()>0)
+        {
+            long long t=pq.top().first;
+            int u=pq.top().second;
             pq.pop();
-
-            for(auto it : adj[node]){
-                if((long long)d+it.second<dist[it.first]){
-                    pq.push({d+it.second,it.first});
-                    dist[it.first]=d+it.second;
-                    no_of_ways[it.first]=no_of_ways[node]%mod;
+            
+            for(auto neg:adj[u])
+            {
+                long long d=t+neg.second;
+                int v=neg.first;
+                if(dist[v]>d)
+                {
+                    dist[v]=d;
+                    count[v]=count[u]%m;
+                    pq.push({d,v});
                 }
-                else if((long long)d+it.second==dist[it.first]){
-                    no_of_ways[it.first]=(no_of_ways[it.first]+no_of_ways[node])%mod;
+                else if(dist[v]==d)
+                {
+                    count[v]+=count[u];
+                    count[v]=count[v]%m;
                 }
+                
             }
         }
-
-        return no_of_ways[n-1]%mod;
+        return count[n-1];
+        
+        
+        
+        
+        
         
     }
 };
